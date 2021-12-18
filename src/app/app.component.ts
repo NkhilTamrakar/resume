3adbac7c42e66658d1aa1bdd11dd75e929755a63
NkhilTamrakar <tamrakarn17@gmail.com>
2021-12-18 17:21:58 +0530

    hehe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import * as AOS from 'aos';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resume';
+  ngOnInit(){
+    AOS.init();
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+import { AboutComponent } from './about/about.component';
+import { AcademicsComponent } from './academics/academics.component';
+import { EducationComponent } from './education/education.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FrontpageComponent,
+    AboutComponent,
+    AcademicsComponent,
+    EducationComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
